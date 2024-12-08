0b834cbccbe11f4bfb678abc0c65893cb268094c
Tedros <tedtfu@gmail.com>
2024-12-07 21:24:47 -0500

    tailwind config

--- webpack.frontend.config.js
+++ webpack.frontend.config.js
@@ -18,8 +18,19 @@ module.exports = {
         use: "ts-loader",
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
       },
     ],
   },
