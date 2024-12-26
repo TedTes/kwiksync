628892a14ee73ca60e48c419caead1ac662e74c7
Tedros <tedtfu@gmail.com>
2024-12-26 18:10:12 -0500

    adding vars

--- src/config/env-variables.ts
+++ src/config/env-variables.ts
@@ -1,6 +1,6 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 export const envVariables = {
-  webServerURL: process.env.APP_SERVER_URL || "http://localhost:3000",
+  webServerURL: process.env.APP_SERVER_URL || "http://localhost:3001",
   accessTokenKey: process.env.ACCESS_TOKEN_KEY || "access_secret",
   refreshTokenKey: process.env.REFRESH_TOKEN_KEY || "refresh_secret",
   magicLinkSecretKey: process.env.MAGIC_LINK_SECRET_KEY || "secret_key",
