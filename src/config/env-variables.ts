a0036b2d7b9e6e71fbbf861f7445042257131a2d
Tedros <tedtfu@gmail.com>
2024-12-27 13:44:36 -0500

    refactoring bug fix

--- src/config/env-variables.ts
+++ src/config/env-variables.ts
@@ -5,11 +5,9 @@ export const envVariables = {
   refreshTokenKey: process.env.REFRESH_TOKEN_KEY || "refresh_secret",
   magicLinkSecretKey: process.env.MAGIC_LINK_SECRET_KEY || "secret_key",
   postgressConfig: {
-    host: process.env.POSTGRES_HOST || "localhost",
-    port: process.env.POSTGRES_PORT || "5432",
-    username: process.env.POSTGRES_USER || "postgres",
-    password: process.env.POSTGRES_PASSWORD || "test",
-    database: process.env.POSTGRES_DB || "kwiksync",
+    url:
+      process.env.POSTGRES_DB_URL ||
+      "postgres://root:test@localhost:5432/kwiksync",
     synchronize: process.env.NODE_ENV !== "production",
     logging: isDevelopment ? "true" : process.env.POSTGRES_LOGGING,
   },
