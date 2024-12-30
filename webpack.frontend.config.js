15b5e27ba7dfd520d567c63866e49b1618739758
Tedros <tedtfu@gmail.com>
2024-12-30 15:21:06 -0500

    removing unused code

--- webpack.frontend.config.js
+++ webpack.frontend.config.js
@@ -46,9 +46,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/web/public/index.html",
     }),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-    }),
     new CopyWebpackPlugin({
       patterns: [
         {
