313f7d7c215889ee595381f609a85a3ae1f069b3
Tedros <tedtfu@gmail.com>
2025-01-06 11:26:20 -0500

    + AWS s3 integration

---
+++ src/utils/s3.ts
@@ -0,0 +1,30 @@
+import { S3Client as S3, PutObjectCommand } from "@aws-sdk/client-s3";
+import { envVariables } from "../config";
+const {
+  configAWS: { AWSaccessKeyId, AWSsecretAccessKey, region },
+} = envVariables;
+
+class S3Client {
+  private s3;
+  constructor() {
+    this.s3 = new S3({
+      region,
+      credentials: {
+        accessKeyId: AWSaccessKeyId,
+        secretAccessKey: AWSsecretAccessKey,
+      },
+    });
+  }
+  upload = async ({ bucket, key, body, mimeType }: s3FileObj) => {
+    const command = new PutObjectCommand({
+      Bucket: bucket,
+      Key: key,
+      Body: body,
+      ContentType: mimeType,
+    });
+
+    return await this.s3.send(command);
+  };
+}
+
+export const s3Client = new S3Client();
