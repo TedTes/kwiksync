746216e7186787aa811eba11233c58bd60e1461f
Tedros <tedtfu@gmail.com>
2025-01-07 12:57:32 -0500

    refactoring

--- src/controllers/sales.controller.ts
+++ src/controllers/sales.controller.ts
@@ -1,12 +1,2 @@
 import { Request, Response } from "express";
 import { fetchWeeklyRevenue } from "../services";
-
-export const getWeeklyRevenue = async (req: Request, res: Response) => {
-  try {
-    const weeklyRevenue = await fetchWeeklyRevenue();
-    res.json(weeklyRevenue);
-  } catch (error) {
-    console.error("Error in weekly revenue controller:", error);
-    res.status(500).json({ error: "Failed to fetch weekly revenue" });
-  }
-};
--- src/services/sales.service.ts
+++ src/services/sales.service.ts
@@ -3,30 +3,6 @@ import { Sales } from "../models";
 
 const salesRepository = AppDataSource.getRepository(Sales);
 
-export const fetchWeeklyRevenue = async () => {
-  try {
-    const weeklyData = await salesRepository.query(`
-        SELECT 
-          to_char(DATE_TRUNC('day', "saleDate"), 'Dy') as day,
-          COALESCE(SUM("totalRevenue"), 0) as revenue
-        FROM sales
-        WHERE "saleDate" >= NOW() - INTERVAL '7 days'
-        GROUP BY DATE_TRUNC('day', "saleDate"), day
-        ORDER BY DATE_TRUNC('day', "saleDate")
-      `);
-
-    const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    return daysOfWeek.map((day) => ({
-      day,
-      revenue: Number(
-        weeklyData.find((d: QueryResult) => d.day === day)?.revenue || 0
-      ),
-    }));
-  } catch (error) {
-    throw new Error("Failed to fetch weekly revenue");
-  }
-};
-
 export const fetchSalesReport = async (startDate: string, endDate: string) => {
   return salesRepository
     .createQueryBuilder("sales")
