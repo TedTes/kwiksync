1ba312ae983ca05c99ec1ec2075ea07682b3a09d
Tedros <tedtfu@gmail.com>
2025-01-17 08:15:16 -0500

    refactoring

--- src/controllers/analytics.controller.ts
+++ src/controllers/analytics.controller.ts
@@ -1,38 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import {
-  fetchInventoryHistory,
-  logInventoryChange,
   fetchSalesReport,
   fetchWeeklyRevenue,
   fetchRecentActivity,
 } from "../services";
-
-export const getInventoryHistory = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const productId = parseInt(req.params.productId);
-    const history = await fetchInventoryHistory(productId);
-
-    res.status(200).json({ productId, history });
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const logInventoryChangeController = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const productId = parseInt(req.params.productId);
-  const quantity = parseInt(req.params.quantity, 10);
-
-  await logInventoryChange(productId, quantity);
-  res.status(200).json();
-};
 
 export const getSalesReport = async (
   req: Request,
