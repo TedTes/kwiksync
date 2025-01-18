8a5152ec7622cd4833d736ff77efb17c6b90e777
Tedros <tedtfu@gmail.com>
2025-01-17 21:23:35 -0500

    add custom logs

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,5 +1,11 @@
 import morgan from "morgan";
+import { Request, Response, NextFunction } from "express";
 
-export const requestLogger = morgan(
-  ":method :url :status :res[content-length] - :response-time ms"
-);
+export const requestLogger = [
+  morgan(":method :url :status :res[content-length] - :response-time ms"),
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log("Headers:", req.headers);
+    console.log("Cookie Header:", req.headers.cookie);
+    next();
+  },
+];
