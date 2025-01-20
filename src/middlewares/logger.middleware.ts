01d0cc16fa1dbc53727bfdba5f5e6e52649741a4
Tedros <tedtfu@gmail.com>
2025-01-20 10:12:00 -0500

    persisting production logs, .gitignore update

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,11 +1,36 @@
 import morgan from "morgan";
 import { Request, Response, NextFunction } from "express";
+import fs from "fs";
+import path from "path";
 
-export const requestLogger = [
-  morgan(":method :url :status :res[content-length] - :response-time ms"),
-  (req: Request, res: Response, next: NextFunction) => {
-    console.log("Headers:", req.headers);
-    console.log("Cookie Header:", req.headers.cookie);
-    next();
-  },
-];
+const getMorganMiddleware = () => {
+  if (process.env.NODE_ENV === "production") {
+    const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+      flags: "a", // Append mode
+    });
+    return morgan("combined", { stream: logStream });
+  } else {
+    return morgan(
+      ":method :url :status :res[content-length] - :response-time ms"
+    );
+  }
+};
+
+const logSafeRequestInfo = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("Request Method:", req.method);
+  console.log("Request URL:", req.url);
+
+  const safeHeaders = {
+    "user-agent": req.headers["user-agent"],
+    host: req.headers["host"],
+  };
+  console.log("Safe Headers:", safeHeaders);
+
+  next();
+};
+
+export const requestLogger = [getMorganMiddleware(), logSafeRequestInfo];
