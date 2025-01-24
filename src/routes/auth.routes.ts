9ef3393142326acd4d123bb3ffc5f46a11c77286
Tedros <tedtfu@gmail.com>
2025-01-23 20:36:42 -0500

    tiktok oauth20 integ

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -15,7 +15,7 @@ import {
   verifyMagicLinkController,
 } from "../controllers";
 import { generateTokens, setCookie, validateHmac } from "../utils";
-import { webServerURL, shopifyAuth } from "../config";
+import { webServerURL, shopifyAuth, tiktokAuth } from "../config";
 export const authRouter = Router();
 
 const allowedOrigin = `${webServerURL}/dashboard`;
@@ -147,9 +147,10 @@ authRouter.get("/shopify/connect", (req, res) => {
 
   const params = new URLSearchParams({
     client_id: shopifyClientId!,
-    scope: `${shopfiyScope}`,
-    redirect_uri: `${shopifyCallbackURL}`,
+    scope: shopfiyScope,
+    redirect_uri: shopifyCallbackURL,
     state,
+    response_type: "code",
   });
   res.redirect(`${shopifyRedirectURL}?${params.toString()}`);
 });
@@ -197,3 +198,52 @@ authRouter.get(
     }
   }
 );
+
+authRouter.get("/tiktok/connect", (req: Request, res: Response) => {
+  const { tiktokClientKey, tiktokScope, tiktokCallbackURL, tiktokAuthURL } =
+    tiktokAuth;
+  const state = crypto.randomBytes(16).toString("hex");
+  const params = new URLSearchParams({
+    client_key: tiktokClientKey!,
+    response_type: "code",
+    scope: tiktokScope!,
+    state,
+    redirect_uri: tiktokCallbackURL,
+  });
+  const authUrl = `${tiktokAuthURL}?${params.toString()}`;
+
+  res.json({ authUrl });
+});
+
+authRouter.get("/tiktok/callback", async (req: Request, res: Response) => {
+  const { code, state } = req.query;
+  const { tiktokClientSecret, tiktokClientKey, tiktokTokenURL } = tiktokAuth;
+  const savedState = req.cookies.tiktokState;
+
+  if (state !== savedState) {
+    throw res.status(403).send("Invalid state");
+  }
+
+  try {
+    const tokenResponse = await axios.post(tiktokTokenURL, {
+      client_key: tiktokClientKey,
+      client_secret: tiktokClientSecret,
+      code,
+      grant_type: "authorization_code",
+    });
+
+    const { access_token, refresh_token } = tokenResponse.data;
+
+    // await MerchantPlatform.create({
+    //   merchantId: req.user.id,
+    //   platform: 'tiktok',
+    //   accessToken: access_token,
+    //   refreshToken: refresh_token,
+    //   isActive: true
+    // });
+
+    res.redirect("/dashboard?connection=success");
+  } catch (error) {
+    res.redirect("/dashboard?connection=failed");
+  }
+});
