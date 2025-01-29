e1ba6291612c2df2981333123ac293ea786d2ec6
Tedros <tedtfu@gmail.com>
2025-01-29 13:32:36 -0500

    validating subscription

--- src/routes/subscription.routes.ts
+++ src/routes/subscription.routes.ts
@@ -1,6 +1,33 @@
 import { Router } from "express";
 import { SubscriptionController } from "../controllers";
-
+import { validateRequest } from "../middlewares";
+import { body } from "express-validator";
 export const subscriptionsRouter = Router();
 
-subscriptionsRouter.get("/", SubscriptionController.create);
+subscriptionsRouter.post(
+  "/",
+  [
+    body("merchantId")
+      .isString()
+      .notEmpty()
+      .withMessage("Merchant ID is required"),
+    body("planId").isString().notEmpty().withMessage("Plan ID is required"),
+    body("paymentMethodDetails")
+      .isObject()
+      .withMessage("Payment method details are required"),
+    body("paymentMethodDetails.cardNumber")
+      .isString()
+      .notEmpty()
+      .withMessage("Card number is required"),
+    body("paymentMethodDetails.expiryDate")
+      .isString()
+      .notEmpty()
+      .withMessage("Expiry date is required"),
+    body("paymentMethodDetails.cvc")
+      .isString()
+      .notEmpty()
+      .withMessage("CVC is required"),
+    validateRequest,
+  ],
+  SubscriptionController.createSubscription
+);
