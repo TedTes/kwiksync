ae472284fdfa7c08d0b6d634e799d31894385b83
Tedros <tedtfu@gmail.com>
2025-01-29 13:34:18 -0500

    subscription service + caching

--- src/interfaces/payment-provider.interface.ts
+++ src/interfaces/payment-provider.interface.ts
@@ -9,22 +9,48 @@ export interface SubscriptionResult {
   periodStart: Date;
   periodEnd: Date;
   metadata?: any;
+  currentPeriodEnd?: Date;
+  paymentMetadata?: any;
 }
 export interface SubscriptionCreateParams {
   planId: number;
   price: number;
   interval: "month" | "year";
 }
+export interface IPaymentMethod {
+  id: string;
+  merchantId: string;
+  provider: string;
+  providerId: string;
+  paymentMethodType: string;
+  cardLast4?: string;
+  expiryDate?: string;
+  cvc?: string;
+  billingAddress?: {
+    street: string;
+    city: string;
+    state?: string;
+    postalCode: string;
+    country: string;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
 export interface PaymentProvider {
   createCustomer(merchantId: string, data: CustomerData): Promise<string>;
   createSubscription(
     customerId: string,
-    params: SubscriptionCreateParams
+    planId: string,
+    paymentMethod: IPaymentMethod
   ): Promise<SubscriptionResult>;
   cancelSubscription(subscriptionId: string): Promise<void>;
   updatePaymentMethod(
     customerId: string,
     paymentMethodId: string
   ): Promise<void>;
+  createPaymentMethod(
+    customerId: string,
+    paymentMethod: IPaymentMethod
+  ): Promise<IPaymentMethod>;
   handleWebhook(payload: any, headers: any): Promise<void>;
 }
--- src/services/payment-service.service.ts
+++ src/services/payment-service.service.ts
@@ -1,11 +1,46 @@
 import { AppDataSource } from "../config";
-import { PaymentProvider } from "../interfaces";
+import { IPaymentMethod, PaymentProvider } from "../interfaces";
 import { StripeProvider } from "../services";
 import { MerchantSubscription, Plan } from "../models";
 
 export class PaymentService {
-  private static provider: { [key: string]: PaymentProvider } = {};
-  public static getProvider(
+  private providerCache: { [key: string]: PaymentProvider } = {};
+  private provider: PaymentProvider;
+  constructor(provider: string, config: PaymentProviderConfig) {
+    this.provider = this.getProvider(provider, config);
+  }
+  public async createSubscription(
+    merchantId: string,
+    planId: string,
+    paymentMethod: any
+  ) {
+    // Logic to create a subscription using the provider's API
+    return await this.provider.createSubscription(
+      merchantId,
+      planId,
+      paymentMethod
+    );
+  }
+  public async createPaymentMethod(
+    merchantId: string,
+    paymentMethodObject: IPaymentMethod
+  ): Promise<IPaymentMethod> {
+    try {
+      // TODO:Logic to create a payment method using the provider's API
+      return await this.provider.createPaymentMethod(
+        merchantId,
+        paymentMethodObject
+      );
+    } catch (error: any) {
+      console.error("Payment method creation error:", error);
+      throw {
+        message:
+          error.message || "An error occurred while creating payment method",
+        status: 500,
+      };
+    }
+  }
+  private getProvider(
     provider: string,
     config: {
       secretKey: string;
@@ -13,16 +48,16 @@ export class PaymentService {
     }
   ): PaymentProvider {
     try {
-      if (!this.provider[provider]) {
+      if (!this.providerCache[provider]) {
         switch (provider.toLowerCase()) {
           case "stripe":
-            this.provider[provider] = new StripeProvider(config);
+            this.providerCache[provider] = new StripeProvider(config);
             break;
           default:
             throw new Error(`Unsupported payment provider: ${provider}`);
         }
       }
-      return this.provider[provider];
+      return this.providerCache[provider];
     } catch (error: any) {
       console.error("Payment provider error:", error);
       throw (
