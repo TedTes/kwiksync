4ee31ce63e503b3a4d89df7906d8f3c450b3745a
Tedros Tesfu <tedtfu@gmail.com>
2025-04-24 08:10:22 -0400

    Update rbac.middleware.ts

--- src/middlewares/rbac.middleware.ts
+++ src/middlewares/rbac.middleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 export const authorizeRoles = (roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      const user = (req as any).user; //  `user` is added to the request by authentication middleware
+      const user = (req as any).user; // // `user` is added to the request by authentication middleware
 
       if (!user) {
         next({ status: 401, message: "Unauthorized: No user found" });
